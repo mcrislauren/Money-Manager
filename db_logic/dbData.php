cb9a4c30733f37f7f86818ee2f718cafb2808dcc
Ovidiu-Florin Bogdan <ovidiu.b13@gmail.com>
2014-08-02 11:45:47 +0300

    Initial DB communication testing - working

--- db_logic/dbData.php
+++ db_logic/dbData.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
 define('USERNAME', 'user');
-define('PASSWROD', 'some_pass');
+define('PASSWORD', 'some_pass');
 define('DATABASE', 'my_money');
 define('PORT','');
 define('SOCKET','');
--- db_logic/dbTesting.php
+++ db_logic/dbTesting.php
@@ -1,21 +1,12 @@
 <?php
-echo "wazuuuup"
 require('dbData.php');
-//    $mysqli = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
-//    if ($mysqli->connect_errno) {
-//        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-//    }
-////    $date = date("Y-m-d H:i:s");
-////    $query = "INSERT INTO dates VALUES('','$date')";
-////    if(mysqli_query($mysqli,$query)){
-////        echo $date." was inserted in db <br>";
-////    }else{
-////        echo "Something went wrong man, check your damn code !";
-////    }
-//    if($result = mysqli->query("Select * from Users"))
-//    {
-//        printf("Num of rows = %d\n", $result->num_rows);
-//        $result->close();
-//    }
-//    mysqli->close();
+$mysqli = new mysqli(HOST, USERNAME, PASSWORD, DATABASE);
+if ($mysqli->connect_errno)
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+if($result = $mysqli->query("Select * from Users"))
+{
+    printf("Num of rows = %d\n", $result->num_rows);
+    $result->close();
+}
+$mysqli->close();
 ?>
